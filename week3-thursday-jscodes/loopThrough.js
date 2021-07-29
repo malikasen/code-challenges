83dd66b6b3bf93262193b0f35ef98f7259bc25c9
Malika <malikasenova@gmail.com>
2021-07-29 11:23:15 -0700

    Add loopThrough script

---
+++ week3-thursday-jscodes/loopThrough.js
@@ -0,0 +1,14 @@
+function loopThrough(num) {
+	let count = 0;
+	while (count < num) {
+		console.log('Hello World');
+		count++;
+	}
+}
+
+loopThrough(0);
+console.log('------------------');
+loopThrough(1);
+console.log('------------------');
+loopThrough(3);
+console.log('------------------');

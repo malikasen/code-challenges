5ac99a99b36e9b5b83a53aa4010d52ce2b52ee9c
Malika <malikasenova@gmail.com>
2021-07-29 11:37:10 -0700

    Add calculateSum script

---
+++ week3-thursday-jscodes/calculateSum.js
@@ -0,0 +1,11 @@
+function calculateSum(num) {
+	if (num === 0) {
+		return 0;
+	}
+	return num + calculateSum(num - 1);
+}
+
+console.log(calculateSum(0));
+console.log(calculateSum(1));
+console.log(calculateSum(3));
+console.log(calculateSum(10));

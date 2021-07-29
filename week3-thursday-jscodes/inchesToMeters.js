a210c716b7813319bed0b57058cb53de1a592338
Malika <malikasenova@gmail.com>
2021-07-29 11:15:58 -0700

    Add week3-thursday-jscodes folder and inchesToMeters script

---
+++ week3-thursday-jscodes/inchesToMeters.js
@@ -0,0 +1,9 @@
+function metersToInches(numOfMeters) {
+	let numOfInches = numOfMeters * 39.3701;
+	return numOfInches;
+}
+
+console.log(metersToInches(0));
+console.log(metersToInches(1));
+console.log(metersToInches(1.5));
+console.log(metersToInches(15.6));

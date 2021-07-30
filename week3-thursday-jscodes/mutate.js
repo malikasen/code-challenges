ce24c8d4aaf92d68d1483b188e569ae15517fa95
Malika <malikasenova@gmail.com>
2021-07-29 17:45:31 -0700

    Add 'mutate' script

---
+++ week3-thursday-jscodes/mutate.js
@@ -0,0 +1,14 @@
+let arr = [{name: 'apple', color: 'green'},{name: 'mango', color: 'yellow'}]
+function mutate(arr) {
+	for (let item of arr) {
+		console.log(item.color);
+	}
+	arr[0].color = 'red';
+	arr[1].color = 'red-green';
+	for (let item of arr) {
+		console.log(item['color']);
+	}
+}
+
+
+mutate(arr);
